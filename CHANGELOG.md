5a3e37abdedaed89206cf99eadf5ab48344e3a59
olaar09 <agboolar09@gmail.com>
2021-08-07 17:06:33 +0100

    can sign up

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
+* Initial implementations of the signup, sign in and forgot screens and functionality
--- lib/src/bloc/register/cubit.dart
+++ lib/src/bloc/register/cubit.dart
@@ -1,6 +1,6 @@
 import 'package:auth_with_firebase/src/repo/fire_auth.dart';
-import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'state.dart';
 
